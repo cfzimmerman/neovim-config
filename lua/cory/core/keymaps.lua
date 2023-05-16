a280d3ff08ee4b51d2e527d7ba46b3ab7b0636bf
rendergame <admin@render.game>
2023-05-16 09:32:21 -0500

    add bd keymap

--- lua/cory/core/keymaps.lua
+++ lua/cory/core/keymaps.lua
@@ -19,6 +19,8 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
 
+keymap.set("n", "<leader>qq", ":w<CR>:bd<CR>")
+
 -- plugin keymaps
 
 -- vim-maximizer
